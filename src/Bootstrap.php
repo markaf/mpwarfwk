b7522a5fbcc0c6aa11e2c090b34d6aac765c1a57
markaf <marcandres92@gmail.com>
2015-03-11 18:05:53 +0100

    added Bootstrap class

---
+++ src/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 11/03/2015
+ * Time: 17:58
+ */
+
+class Bootstrap {
+
+    public function __construct(){
+        echo "Hola mundo!";
+    }
+} 
