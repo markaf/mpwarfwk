83a3551de38373b09c614207929be9738b27d401
markaf <marcandres92@gmail.com>
2015-03-21 17:54:53 +0100

    added Container.php file

---
+++ src/Mpwarfwk/Component/Container/Container.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 18/03/2015
+ * Time: 19:24
+ */
+
+class Container {
+
+
+    public function get($service){
+        include '../src/Conf/Services.php';
+$arguments = array();
+
+if(!empty($config[$service])){
+    if(!empty config arguments)
+    foreach
+}
+    }
+} 
