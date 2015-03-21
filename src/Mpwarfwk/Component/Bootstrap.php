c6a576adefa032145275cfb41178ae6bffc90acc
markaf <marcandres92@gmail.com>
2015-03-21 17:33:48 +0100

    changes in bootstrap.php

--- src/Mpwarfwk/Component/Bootstrap.php
+++ src/Mpwarfwk/Component/Bootstrap.php
@@ -8,14 +8,25 @@
 
 namespace Mpwarfwk\Component;
 
+use Mpwarfwk\Component\Request\Request;
+use Mpwarfwk\Component\Routing\Routing;
+use Mpwarfwk\Component\Session\Session;
+use Mpwarfwk\Component\Routing\Route;
+
 class Bootstrap {
 
     public function __construct(){
-        echo "Hola mundo!";
+        //echo "Hola mundo!";
     }
     public function run(){
+        $session = new Session();
+        $request = new Request($session);
         $routing = new Routing();
-        $routeController = $routing->getRouteController("/home");
+        $routeController = $routing->getRouteController($request);
         $controller = new $routeController();
+    }
+
+    public function executeController(Route $a, Request $b){
+
     }
 }
