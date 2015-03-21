26dfe917a9f90c82797e7c90a3b1b79fdaca4915
markaf <marcandres92@gmail.com>
2015-03-21 17:48:58 +0100

    added HttpResponse.php and JasonResponse.php files

---
+++ src/Mpwarfwk/Component/Response/HttpResponse.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 16/03/2015
+ * Time: 19:40
+ */
+
+class HttpdResponse
+{
+
+    protected $contrent
+    status
+
+__construct($content, status = 200)
+
+    send(){
+    if($thid->status != 200){
+        header("HTTD/1.0 404 Not Found");
+    }
+
+echo $this->content;
+}
+}
---
+++ src/Mpwarfwk/Component/Response/JasonResponse.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 16/03/2015
+ * Time: 19:42
+ */
+
+class JasonResponse extends Response {
+send
+header('Content-Type: application/jason');
+
+//si no es array
+$thid->content = array($this->content)
+
+} 
